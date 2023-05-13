46ac60d027f81e71fcd7e005b4a211d4dc0440f9
Alyona-Nekhryshchyniuk <o.nekhryshchyniuk@gmail.com>
2023-05-13 09:48:56 +0300

    user endpoints are ready

--- app.js
+++ app.js
@@ -14,10 +14,13 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 // app.set("view engine", "ejs");
 
 // app.use("/api/pets", petsRouter);
 app.use("/api/auth", authRouter);
+app.use("/update", authRouter);
+
 // app.use("/", authRouter);
 // app.use("/", express.static("public"));
 
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,20 +1,38 @@
-const express = require("express");
-const router = new express.Router();
-const tryCatchMiddleware = require("../../middlewares/tryCatchMiddleware");
-// const isTokenValidMiddleware = require("../../middlewares/isTokenValidMiddleware");
-// const multer = require("multer");
-// const path = require("path");
-
 const {
   registerController,
   loginController,
   logoutController,
+  updateController,
 } = require("../../controllers/userControllers");
+const express = require("express");
+const router = new express.Router();
+const tryCatchMiddleware = require("../../middlewares/tryCatchMiddleware");
+const isTokenValidMiddleware = require("../../middlewares/isTokenValidMiddleware");
+const path = require("path");
+const pathToTemporaryFolder = path.resolve("temp");
+const multer = require("multer");
+console.log(pathToTemporaryFolder);
+const multerConfig = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, pathToTemporaryFolder);
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: multerConfig });
 
 router.post("/register", tryCatchMiddleware(registerController));
 
 router.post("/login", tryCatchMiddleware(loginController));
 
 router.post("/logout", tryCatchMiddleware(logoutController));
+
+router.patch(
+  "/avatar",
+  upload.single("avatar"),
+  isTokenValidMiddleware(tryCatchMiddleware(updateController))
+);
 
 module.exports = { authRouter: router };
