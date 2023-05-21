f1cae5cfeb88171e0e64f227b775429462715868
Alyona-Nekhryshchyniuk <o.nekhryshchyniuk@gmail.com>
2023-05-21 09:53:14 +0300

    d

--- models/petShema.js
+++ models/petShema.js
@@ -10,16 +10,16 @@ const { Schema, model } = mongoose;
 
 const petSchema = new Schema(
   {
-    // name: {
-    //   type: String,
-    //   minLength: 2,
-    //   maxLength: 16,
-    //   match: [
-    //     /^[a-zA-Zа-яА-ЯіІїЇґҐ]+(?: [a-zA-Zа-яА-ЯіІїЇґҐ]+)*$/,
-    //     "Only letters can be accepted",
-    //   ],
-    //   required: [true, "Name is required"],
-    // },
+    name: {
+      type: String,
+      minLength: 2,
+      maxLength: 16,
+      match: [
+        /^[a-zA-Zа-яА-ЯіІїЇґҐ]+(?: [a-zA-Zа-яА-ЯіІїЇґҐ]+)*$/,
+        "Only letters can be accepted",
+      ],
+      required: [true, "Name is required"],
+    },
     owner: {
       type: Schema.Types.ObjectId,
       ref: "user",
@@ -52,14 +52,14 @@ const petSchema = new Schema(
 const Pet = model("Pet", petSchema);
 
 const petJOISchema = Joi.object({
-  // name: Joi.string()
-  //   .min(2)
-  //   .max(16)
-  //   .regex(
-  //     /^[a-zA-Zа-яА-ЯіІїЇґҐ]+(?: [a-zA-Zа-яА-ЯіІїЇґҐ]+)*$/,
-  //     "Only letters can be accepted"
-  //   )
-  //   .required(),
+  name: Joi.string()
+    .min(2)
+    .max(16)
+    .regex(
+      /^[a-zA-Zа-яА-ЯіІїЇґҐ]+(?: [a-zA-Zа-яА-ЯіІїЇґҐ]+)*$/,
+      "Only letters can be accepted"
+    )
+    .required(),
   // birthDate: Joi.date().less(Date.now()).format("DD.MM.YYYY").required(),
   // breed: Joi.string()
   //   .min(2)
